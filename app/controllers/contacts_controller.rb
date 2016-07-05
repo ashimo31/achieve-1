9cd06e0f5597c5dbb7966f429e3d0fcf962eeecf
Nobuo Mori <nobuom@gmail.com>
2016-07-05 07:33:42 +0000

    first commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,63 @@
+class ContactsController < ApplicationController
+ 
+before_action :set_contact, only: [:edit, :update, :destroy]
+  
+  def index
+    @contacts = Contact.all
+  end
+  
+  def new
+    if params[:back]
+      @contact = Contact.new(contacts_params)
+    else
+      @contact= Contact.new
+    end
+  end
+  
+  def create
+    @contact=Contact.new(contacts_params)
+    if @contact.save
+      redirect_to root_path, notice: "お問い合わせを受け付けました！"
+    else
+      render action: 'new'
+    end
+  end
+  
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+      
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contacts_params)
+      redirect_to contacts_path, notice: "お問い合わせを更新しました！"
+    else
+      render action: 'edit'
+    end
+  end
+  
+  
+  def confirm
+    @contact = Contact.new(contacts_params)
+    render :new if @contact.invalid?
+  end
+  
+  
+  def destroy
+    @contact=Contact.find(params[:id])
+    if @contact.destroy
+      redirect_to contacts_path, notice: "お問い合わせを削除しました！"
+    else
+      render action: 'new'
+    end
+  end
+  
+  private 
+    def contacts_params
+      params.require(:contact).permit(:name, :email, :content)
+    end
+    
+    def set_contact
+    @contact = Contact.find(params[:id])
+    end 
+end
