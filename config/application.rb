f1a27c75f893e299b319677d1f4a7298274cecef
ashimo31 <nobuom@MoriNobuos-MacBook-Air.local>
2016-08-13 17:13:58 +0900

    fix config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
-require 'devise'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -24,7 +21,7 @@ module Achieve
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
+
     config.action_view.field_error_proc = proc {|html_tag, instance| html_tag}
   end
 end
